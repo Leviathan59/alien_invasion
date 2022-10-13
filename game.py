a34a741d8659440521a4613467ead63486b177d5
Leviathan59 <115638538+Leviathan59@users.noreply.github.com>
2022-10-13 09:36:48 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,27 @@
+import sys
+
 import pygame
 
-print("hello world")
-print("starting alien game")
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initializing the game, and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set.mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            #Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #make the most recently drawn screen visisble.
+            pygame.display.flip()
+if __name__=='__main__':
+    #make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
