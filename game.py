178a380fe5709de090cfeb941a0f3ec13c401229
Leviathan59 <115638538+Leviathan59@users.noreply.github.com>
2022-10-13 09:43:16 -0400

    Update game.py

--- game.py
+++ game.py
@@ -12,6 +12,9 @@ class AlienInvasion:
         self.screen = pygame.display.set.mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
 
+        #set the background color
+        self.bg_color = (230,230,230)
+
     def run_game(self):
         """Start the main loop for the game"""
         while True:
@@ -19,9 +22,13 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            #Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
             #make the most recently drawn screen visisble.
             pygame.display.flip()
 if __name__=='__main__':
     #make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
+
