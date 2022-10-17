9a8f17a286449857a838135453e437d94ec9f149
Leviathan59 <115638538+Leviathan59@users.noreply.github.com>
2022-10-16 20:02:23 -0400

    Update game.py

--- game.py
+++ game.py
@@ -35,12 +35,20 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                def _check_keydown_events(self, event):
+                        """Respond to keypresses."""
                     if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = True
                     elif event.key == pygame.K_LEFT:
                         self.ship.moving_left = True
+                    elif event.key == pygame.K_q:
+                        sys.exit()
 
                     elif event.type == pygame.KEYUP:
+                        self._check_keyup_events(event)
+                def _check_keyup_events(self, event):
+                    """Respond to releases."""
                     if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = False
                     elif event.key == pygame.K_LEFT:
