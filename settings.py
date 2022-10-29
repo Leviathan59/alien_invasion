e766bf59be43314bd25bd4e05098a95a8605033c
Leviathan59 <115638538+Leviathan59@users.noreply.github.com>
2022-10-29 13:39:23 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,10 +20,11 @@ def __init__(self):
     self.bg_color = (230, 230, 230)
 
     #Ship Settings
-    self.ship_speed = 2.0
+    self.ship_speed = 1.5
+    self.ship_limit = 3
 
     # Bullet settings
-    self.bullet_speed = 1.0
+    self.bullet_speed = 1.5
     self.bullet_width = 3
     self.bullet_height = 15
     self.bullet_color = (60,60,60)
