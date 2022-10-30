c92a61290280de618fdecb69a18070fba9ed03e0
Leviathan59 <115638538+Leviathan59@users.noreply.github.com>
2022-10-30 12:09:53 -0400

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,8 @@ class GameStats:
         """Initialize stats"""
         self.settings = ai_game.settings
         self.reset_stats()
+        # start alien invasion in an active state
+        self.game_active = True
 
     def reset_stats(self):
         """Initialize stats that can change during the game"""
